66eaeb36491e0db8ddfc7ed49a5e391210c6efc4
Joseph Sagastegui <51385039+joss30sg@users.noreply.github.com>
2025-03-06 10:43:08 -0500

    Delete src directory

--- src/main/java/com/InterviewJosephSagastegui/springboot/app/models/dao/IEstudianteDao.java
+++
@@ -1,8 +0,0 @@
-package com.InterviewJosephSagastegui.springboot.app.models.dao;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import com.InterviewJosephSagastegui.springboot.app.models.entity.Estudiantes;
-
-public interface IEstudianteDao extends PagingAndSortingRepository<Estudiantes, Long> {
-
-}
