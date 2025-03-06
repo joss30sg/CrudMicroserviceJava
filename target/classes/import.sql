237ee1692c6bb2cf10f683e9eeb242e367578179
Joseph Sagastegui <51385039+joss30sg@users.noreply.github.com>
2025-03-06 10:55:17 -0500

    Add files via upload

---
+++ target/classes/import.sql
@@ -0,0 +1,19 @@
+/* Populate tables */
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Rafael', 'Robles', 'rafuchoaleman@hotmail.com', 18, '2024', 18);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Carlos', 'Robles', 'carlosherrera@hotmail.com', 15, '2024', 15);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Milagros', 'Torres', 'Mtorres@hotmail.com', 11, '2024', 11);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Hector', 'Salas', 'Hsalas@hotmail.com', 13, '2024', 12);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Carla', 'Salazar', 'Csalazar@hotmail.com', 11, '2024', 11);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Vanesa', 'Lara', 'Vlara@hotmail.com', 11, '2024', 15);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Carlos', 'Perez', 'CPerez@hotmail.com', 12, '2024', 11);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Melissa', 'Barrera', 'MBelisa@hotmail.com', 11, '2024', 11);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Jose', 'Rojas', 'Jrojas@hotmail.com', 14, '2024', 13);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Luis', 'Fernandez', 'LFernandez@hotmail.com', 13, '2024', 12);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Luisa', 'Santiago', 'LSantiago@hotmail.com', 18, '2024', 17);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Veronica', 'Jara', 'VJara@hotmail.com', 15, '2024', 15);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Sofia', 'Herrera', 'SHerrera@hotmail.com', 19, '2024', 19);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Luis', 'Fernandez', 'LFernandez@hotmail.com', 11, '2024', 11);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Liz', 'Sanchez', 'Lsanchez@hotmail.com', 14, '2024', 14);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Roberto', 'Andrade', 'RAndrade@hotmail.com', 15, '2024', 15);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Raul', 'Garcia', 'rgarcia@hotmail.com', 13, '2024', 13);
+INSERT INTO estudiantes (nombre, apellido, email, creditos, semestre, promedio) VALUES('Rocio', 'Duran', 'rduran@hotmail.com', 12, '2024', 12);
